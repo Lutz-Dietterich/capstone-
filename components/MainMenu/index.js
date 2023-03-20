a186a835af30fc2c8a1be36f8dbaec22bd064f28
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-20 15:47:43 +0100

    add active styling menu icons

--- components/MainMenu/index.js
+++ components/MainMenu/index.js
@@ -2,18 +2,26 @@ import styled from "styled-components";
 import { BsHouseDoor } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function MainMenu() {
+  const router = useRouter();
   return (
     <StyledNav>
       <StyledList>
         <StyledListItem>
-          <StyledLink href={"/"}>
+          <StyledLink
+            href={"/"}
+            className={router.pathname === "/" ? "active" : ""}
+          >
             <BsHouseDoor />
           </StyledLink>
         </StyledListItem>
         <StyledListItem>
-          <StyledLink href={"/favourites"}>
+          <StyledLink
+            href={"/favourites"}
+            className={router.pathname === "/favourites" ? "active" : ""}
+          >
             <AiOutlineHeart />
           </StyledLink>
         </StyledListItem>
@@ -47,13 +55,12 @@ const StyledListItem = styled.li`
 `;
 
 const StyledLink = styled(Link)`
-  color: #000;
+  color: #989898;
   text-decoration: none;
   font-size: 2rem;
   font-weight: 600;
 
   &.active {
-    color: #fff;
-    background-color: #000;
+    color: #000;
   }
 `;
