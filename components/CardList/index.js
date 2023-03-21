ba0b5312aae485f201e5cb26d1ad4cce9c82d503
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-21 14:22:56 +0100

    styled card

---
+++ components/CardList/index.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export default function CardList() {
+  return (
+    <>
+      <StyledCard>
+        <StyledCardHeader>Card 1</StyledCardHeader>
+      </StyledCard>
+    </>
+  );
+}
+
+const StyledCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: #fff;
+  border-radius: 30px;
+  border: 1px solid #000;
+`;
+
+const StyledCardHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 30px;
+`;
+
+const styledCardImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+`;
