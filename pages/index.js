191de4590c4af4fa13491c48c16bd5aa1a56474d
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-04-13 17:53:58 +0200

    without comment

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,12 @@
 //   );
 // }
 
-// import { lazy, Suspense } from "react";
-// import useScrollPosition from "../utils/hooks/useScrollPosition";
-// import ScrollToTopButton from "../components/ScrollToTopButton";
-// import LoadingSpinner from "../components/LoadingSpinner";
-
-// const LazyCardList = lazy(() => import("../components/CardList"));
-
-// export default function Home() {
-//   useScrollPosition();
-
-//   return (
-//     <>
-//       <Suspense fallback={<LoadingSpinner />}>
-//         <LazyCardList />
-//       </Suspense>
-//       <ScrollToTopButton />
-//     </>
-//   );
-// }
-
 import { lazy, Suspense } from "react";
 import useScrollPosition from "../utils/hooks/useScrollPosition";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import LoadingSpinner from "../components/LoadingSpinner";
 
-const LazyCardList = lazy(() =>
-  import(
-    /* webpackChunkName: "cardlist" */
-    /* webpackPrefetch: true */
-    "../components/CardList"
-  )
-);
-
-// comment
+const LazyCardList = lazy(() => import("../components/CardList"));
 
 export default function Home() {
   useScrollPosition();
